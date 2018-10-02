80db65496e5cd6343a7304128afc81522f1e51a8
github username <github email address>
2018-10-02 13:49:13 -0400

    worked on frontend

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  let submitForm = document.querySelector("#signUpForm")
-  const url = `http://localhost:3000`
 
-  submitForm.addEventListener("submit", (e) => {
+  const url = "http://localhost:3000/users"
+  const signIn = document.getElementById('sign-in')
+  const signUp = document.getElementById("signUpForm")
+
+
+
+  signUp.addEventListener("submit", (e) => {
     e.preventDefault()
     let nameInput = e.target.name.value
 
-    body = {
+    let newInfo = {
       name: nameInput
     }
+    postUser(newInfo)
+  })
 
+  signIn.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let nameInput = e.target.name.value
+    getUser(nameInput)
+  })
+
+
+
+    function postUser(body){
     fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(body)
-    })
-  })
+    }).then(res => res.json())
+    .then(res => renderProfile(res))
+    //************** NEED TO FIGURE OUT HOW TO FIELD AN ERROR********************//
+    // .catch(function(){
+    //   debugger
+    //   console.log('Error:')
+    // })
+  }
+
+  function renderProfile(user){
+    signIn.style.display = "none"
+    debugger
+    let userName = document.getElementById("name")
+    userName.innerText = user.name
+
+    if (user.paintings){
+      user.paintings.forEach(painting => (){
+        painting
+      })
+    }
+
+  }
+
+
+
+
+
+
+
+
 
 
 })
